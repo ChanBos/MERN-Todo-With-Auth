8033a1ff19717ae9428bdef0e3719df13fb2a08d
Chanelle <scheeperschanelle@gmail.com>
2021-09-01 18:08:50 +0200

    Updated server.js for Deployment

--- server.js
+++ server.js
@@ -17,8 +17,6 @@ const dbConfig = require("./config/app.config.js");
 // Requiring the routes index.js file.
 const todoRoutes = require("./routes/todoRoutes.js");
 const usersRoutes = require("./routes/usersRoutes.js");
-// Requiring Passport.
-const passport = require("passport");
 
 /**
  * Enabling App usages.
@@ -29,7 +27,7 @@ const passport = require("passport");
 // app.use(passport.session());
 
 // Included the body-parser middleware so that the Express server is able to access content that is passed in the body of the HTTP request.
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Enabling middleware to allow server to access MongoDB. To accept requests to the body in .json format.
@@ -38,8 +36,12 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
-// Enabling app to use Helmet to secure the code.
-app.use(helmet());
+// Enabling app to use Helmet to secure the code. Disabled the `contentSecurityPolicy` middleware (keeps the rest) due to inline script errors.
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 
 // Enabling the connection to MongoDB via the uri from the config file.
 // Added useNewUrlParser flag to allow falling back to the old parser should a bug be found in the new parser.
@@ -67,17 +69,14 @@ mongoose.connection.once("open", function () {
 app.use("/todos", todoRoutes);
 app.use("/users", usersRoutes);
 
-// ... other imports 
-const path = require("path")
-
-// ... other app.use middleware 
-app.use(express.static(path.join(__dirname, "client", "build")))
-
-// ...
-// Right before your app.listen(), add this:
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+// Checking if the process is production mode and set for the index.html file from the build folder to be utilized, instead of the public folder.
+const path = require("path");
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Specified to listen to port 8080's HTTP requests. Modified the port code in order to deploy the app to Heroku.
 // Logging a response to the console to confirm that the server is listening to port 8080.
